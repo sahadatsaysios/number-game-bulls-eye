79dafe76300f8417224e38292c7d955e8390a11b
Sahadat  Hossain <sahadathossain@Sahadats-Mac.local>
2018-10-16 03:25:34 -0700

    This is The Bulls Eye number game. completed some steps

---
+++ BullsEyes/AboutController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutController.swift
+//  BullsEyes
+//
+//  Created by Sahadat  Hossain on 11/10/18.
+//  Copyright © 2018 Sahadat  Hossain. All rights reserved.
+//
+
+import UIKit
+
+class AboutController: UIViewController {
+    
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
--- BullsEyes/ViewController.swift
+++ BullsEyes/ViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue : Int = 0
+    @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var lblRandomNumber : UILabel!
+    var randomNumber : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRandomNumber()
+        updateLbl()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func startNewRandomNumber () {
+        randomNumber = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    @IBAction func slideMoved ( _ slider : UISlider){
+        
+        currentValue = lroundf(slider.value)
+        
+    }
 
+    @IBAction func showAlert (){
+        
+        var different : Int
+        if currentValue > randomNumber {
+            different = currentValue - randomNumber
+        }else if currentValue < randomNumber {
+            different = randomNumber - currentValue
+        }else{
+            different = 0
+        }
+        
+        let message = "Current value is : \(currentValue)" + "\nRandom Value \(randomNumber)" + "\nThe differnt is \(different)"
+        let alert = UIAlertController(title: "This is Alert Title", message: message, preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "Good Job", style: .default, handler: nil)
+        
+        alert.addAction(alertAction)
+        
+        present(alert, animated: true, completion: nil)
+        startNewRandomNumber()
+        updateLbl()
+    }
+    func updateLbl() {
+        lblRandomNumber.text = String(randomNumber)
+    }
+    
+    @IBAction func showInfo (){
+        let alert = UIAlertController(title: "Information", message: "Bull's Eye is a simple game. which is developed by Md Sahadat.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Thanks", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
